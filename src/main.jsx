eb3c5a574030ef7d9234baf995440b7c4400615c
suspicious115478 <sus24picious45@gmail.com>
2025-12-30 13:12:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./styles/main.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SuperAdmin from "./pages/SuperAdmin";
+import AdminAgents from "./pages/AdminAgents";
+import AgentHistoryPage from "./pages/AgentHistoryPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<SuperAdmin />} />
+
+        {/* admin -> agents */}
+        <Route path="/admin/:adminId/agents" element={<AdminAgents />} />
+
+        {/* agent -> history */}
+        <Route
+          path="/admin/:adminId/agent/:agentId/history"
+          element={<AgentHistoryPage />}
+        />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
